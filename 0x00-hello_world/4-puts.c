562d7ca5a1f9d6497d20d9ab1ed2df5e710f5bd2
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-08 16:00:42 -0700

    Fix 3 :  a C program that print a sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*A function that print a sentence with puts*/
+
+/**
+ * main - Print a predefined sentence
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
