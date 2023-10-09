e48c5b44631bf96c36b4b13cc4f8634b927b999f
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-09 06:24:56 -0700

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 as end value
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 58 ; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
