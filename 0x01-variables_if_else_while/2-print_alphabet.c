bf1583233387f4c13e64422650990607d97ebedc
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-09 06:02:18 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 as end value
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i < 123 ; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
