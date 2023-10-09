31d1e4ccb2f12d7a7e2a11ac136df55c920cb715
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-09 03:17:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: 0 as end value
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,\
-	2015-10-19\n");
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
 	return (1);
 }
