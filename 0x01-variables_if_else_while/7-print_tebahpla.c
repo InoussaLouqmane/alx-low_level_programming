e5e8c4d66547c0b27d7f6003266f966ab05e34fe
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-09 06:26:42 -0700

    .

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 as end value
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 122; i >= 97 ; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
