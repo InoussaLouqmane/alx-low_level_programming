cc0f18ffc404b0b16502573a57f33e17a0965433
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-10 05:06:10 -0700

    .

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program just
+ * print '_putchar' ;
+ * Return: 0 as end value;
+ */
+int main(void)
+{
+	printf("_putchar\n");
+}
