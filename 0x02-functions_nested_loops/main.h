e3fd889d97c91965fca49a5439f3d50cc12d991b
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-10 09:51:45 -0700

    .

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * _putchar - print characters on screen
+ * @c: the character to print
+ *
+ * Return: 1 if no occurs, 0 otherwise.
+ */
+int _putchar (char c);
