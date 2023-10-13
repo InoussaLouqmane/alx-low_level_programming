ae8e261b849e4040626decef10308133ce840d46
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-14 00:40:36 +0100

    Print the largest of three integers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
-
-void positive_or_negative(int i);
+/**
+ * largest_number - Print the most larger of three int
+ * @a: First Number
+ * @b: Second Number
+ * @c: Third Number
+ *
+ * Return: The largest number
+ */
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
