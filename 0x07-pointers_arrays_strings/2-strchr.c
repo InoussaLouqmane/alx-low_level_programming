e6a65e8c0d58a0959127cd24676cd2bc30824799
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-24 22:17:48 +0100

    Inserting pointer variable in function strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _strchr - a function that locate a character in a string
  * @s: A pointer to a string
@@ -11,14 +12,15 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
-
+	char *ptr = NULL;
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-		{
-			return (s + i);
+		{	
+			ptr = s + i;
+			return (ptr);
 		}
 		i++;
 	}
-	return (NULL);
+	return (ptr);
 }
