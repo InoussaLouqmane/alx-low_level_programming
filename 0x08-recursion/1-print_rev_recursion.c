cb2aa8f61ccff3c1efa5cb481e5b9039b7c279e8
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-25 19:18:45 +0100

    Updating betty Coding Style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,13 +2,13 @@
 /**
  * _print_rev_recursion - print a string in reverse
  * @s: the string to be reversed
- * 
+ *
  * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
