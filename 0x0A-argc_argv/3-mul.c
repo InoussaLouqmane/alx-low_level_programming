2007873a14c4334f6ed8cbf1f572dacac6bfbcdc
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-10-28 04:56:36 +0100

    A program that print multiplication of two integer passed into parameter

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Number of argc
+ * @argv: Arg list
+ *
+ * Return: 0 on success
+ * anything else on failure
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (EXIT_SUCCESS);
+}
