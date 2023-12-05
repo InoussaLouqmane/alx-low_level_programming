74f79cffe2efb27c314accf6543ef3143a062e50
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-12-05 11:36:26 +0100

    alloc_grid() function fix : Handling betty coding style

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,18 +18,18 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = (int **)malloc(sizeof(int *) * height);
-	
+	grid = malloc(sizeof(int *) * height);
+
 	if (grid == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < height; i++)
 	{
-		grid[i] =(int *) malloc(sizeof(int) * width);
-		
+		grid[i] =  malloc(sizeof(int) * width);
+
 		if (grid[i] == NULL)
 			return (NULL);
-		
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
