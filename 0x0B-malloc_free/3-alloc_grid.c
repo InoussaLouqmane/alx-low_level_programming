d2b4913ec0ff6d33312daa45620cb113b1648ead
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-12-05 11:26:01 +0100

    alloc_grid() function fix : Handling malloc failure

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,10 +19,17 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	grid = (int **)malloc(sizeof(int *) * height);
-
+	
+	if (grid == NULL)
+		return (NULL);
+	
 	for (i = 0; i < height; i++)
 	{
 		grid[i] =(int *) malloc(sizeof(int) * width);
+		
+		if (grid[i] == NULL)
+			return (NULL);
+		
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
