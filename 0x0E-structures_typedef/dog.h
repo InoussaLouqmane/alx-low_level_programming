c4d6da95f1755340264066dff7fe80d1bdc16b2d
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-12-07 01:27:44 +0100

    A header file with struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * struct dog - to describe a dog
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* _MAIN_H_ */
