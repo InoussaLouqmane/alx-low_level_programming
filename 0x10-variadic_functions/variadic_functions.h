a9edf9303598815db29d65c6a0539a87f787769e
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-12-14 15:50:41 +0100

    print_numbers a function that prints numbers, followed by a new line

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,4 +2,6 @@
 #define _MAIN_H_
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...)__attribute__((unused));
+void print_numbers(const char *separator,
+		const unsigned int n, ...)__attribute__((unused));
 #endif /* _MAIN_H_ */
