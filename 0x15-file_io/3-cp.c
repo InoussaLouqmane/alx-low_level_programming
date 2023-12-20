86139a503333113c9823e3fb1577f5c79706221b
InoussaLouqmane <inoussalouqmane@gmail.com>
2023-12-20 01:10:41 +0100

    cp function fix : fractionning code into other functions for more readability

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,42 @@
 #include "main.h"
 
 /**
+ * read_err - terminate program after read error
+ * @:filename: the name of the file
+ *
+ * Return: void
+ */
+void read_err(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
+}
+
+/**
+ * write_err - terminate program after write error
+ * @:filename: the name of the file
+ *
+ * Return: void
+ */
+void write_err(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
+}
+
+/**
+ * close_error - terminate program after close error
+ * @fd: the file descriptor value
+ *
+ * Return: void
+ */
+void close_err(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
  * main - Entry point
  * @argc: number of argument passed to function
  * @argv: table of arguments
@@ -23,33 +59,29 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	read_fd = open(argv[1], O_RDONLY);
+	if (read_fd == -1)
+		read_err(argv[1]);
 
 	writen_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 
+	if (writen_fd == -1)
+		write_err(argv[2]);
+
 	while ((read_bytes = read(read_fd, buffer, sizeof(buffer))) > 0)
 	{
-		if (read_fd == -1 || read_bytes == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+		if (read_bytes == -1)
+			read_err(argv[1]);
+
 		writen_bytes = write(writen_fd, buffer, read_bytes);
-		if (writen_bytes == -1 || writen_fd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+
+		if (writen_bytes == -1)
+			write_err(argv[2]);
 	}
 
 	if (close(read_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", read_fd);
-		exit(100);
-	}
+		close_err(read_fd);
+
 	if (close(writen_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", writen_fd);
-		exit(100);
-	}
+		close_err(writen_fd);
 	exit(EXIT_SUCCESS);
 }
