249c55bb56d86a24f78d0276409845b6f026401f
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-01-19 09:05:19 +0100

    More exercises about malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: array length
+ * @size: size of array elements
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		*((char *)ptr + i) = 0;
+
+	return (ptr);
+}
