36c5c8e2c2798083c8d5d06a734ecccc987a3b44
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-01-29 03:44:04 +0100

    variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+/**
+ * vaint - print an integer
+ * @i: the integer to be used
+ *
+ * Return: Always void
+ */
+void vaint(int i)
+{
+	printf("%d", i);
+}
+
+
+/**
+ * vachar - print a character
+ * @c: the character  to be used
+ *
+ * Return: Always void
+ */
+void vachar(char c)
+{
+	printf("%c", c);
+}
+
+
+/**
+ * vafloat - print a float
+ * @f: the float to be used
+ *
+ * Return: Always void
+ */
+
+void vafloat(float f)
+{
+	printf("%f", f);
+}
+
+/**
+ * vastr - print a string
+ * @str: the string to be used
+ *
+ * Return: Always void
+ */
+void vastr(char *str)
+{
+	if (str != NULL)
+	{
+		printf("%s", str);
+		return;
+	}
+	printf("(nil)");
+}
+
+/**
+ * print_all - a function that prints anything
+ * @format: a list of types of arguments passed
+ * to the function
+ * @...: optional arguments
+ *
+ * Return: Void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int len = 0;
+	int good = 0;
+
+	va_start(ap, format);
+
+	while ((format != NULL) && (format[len] != '\0'))
+	{
+		good = 0;
+		switch (format[len])
+		{
+			case 'c':
+				vachar(va_arg(ap, int));
+				good = 1;
+				break;
+			case 'i':
+				vaint(va_arg(ap, int));
+				good = 1;
+				break;
+			case 'f':
+				vafloat(va_arg(ap, double));
+				good = 1;
+				break;
+			case  's':
+				vastr(va_arg(ap, char *));
+				good = 1;
+				break;
+			default:
+				break;
+		}
+		if (format[len + 1] != '\0' && (good == 1))
+			printf(", ");
+		len++;
+	}
+	printf("\n");
+	va_end(ap);
+}
