2fdace359748f89e9aaf096d7a5c9e2edefd9919
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-02-24 17:36:11 +0100

    Fixing Betty Coding Style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,18 @@ size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-		else 
+		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		counter++;
 	}
-	return(counter);
+	return (counter);
 }
